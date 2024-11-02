83a570eb00d57eab53ba24bee34fffc1aa5a4b5f
afluffysquirrel <afluffysquirrel@gmail.com>
2024-11-02 00:10:39 +0000

    feat: hybrid render for contact page

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import type { AstroUserConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 const config: AstroUserConfig = defineConfig({
+	output: "hybrid",
+	adapter: netlify({}),
 	site: "http://www.example.com",
 	integrations: [
 		tailwind({
