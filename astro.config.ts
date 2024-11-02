bad326f062c7b45e0ccdeab61501cc73eab5a5d1
afluffysquirrel <afluffysquirrel@gmail.com>
2024-11-02 00:22:41 +0000

    Revert "feat: hybrid render for contact page"
    This reverts commit 83a570eb00d57eab53ba24bee34fffc1aa5a4b5f.

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import type { AstroUserConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
 
 const config: AstroUserConfig = defineConfig({
-	output: "hybrid",
-	adapter: netlify({}),
 	site: "http://www.example.com",
 	integrations: [
 		tailwind({
